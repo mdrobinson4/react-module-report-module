08bc9ea0a83602a99f674869791d3bec7cb2d028
Jacob Varner <jacob.varner0@gmail.com>
2018-09-12 12:05:27 -0500

    Initial commit

---
+++ src/components/report-module/Dropdown.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Dropdown extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            dropdownData: {
+                graphType: []
+            }
+        }
+    }
+}
