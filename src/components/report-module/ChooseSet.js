dcfca3b21132b43ac51c4f85454979006fc0f19a
mdrobinson4 <mdrobinson4@crimson.ua.edu>
2018-10-22 13:36:18 -0500

    Still issues with limitling slices

--- src/components/report-module/ChooseSet.js
+++ src/components/report-module/ChooseSet.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import update from 'immutability-helper';
 
-/*
-  JARGON -> (1) d2Index --- first set of data (array of values) selected (2) d3Index --- Second set of data (array of values) selected
-  For every element in the array at d2Index, Find another element in the array at d3Index.
-  Each chart represents each (unique) item in the array at d2Index.
-*/
-
 export default class ChooseSet extends React.Component {
     constructor(props) {
       super(props);
@@ -24,14 +18,11 @@ export default class ChooseSet extends React.Component {
 
     render() {
       let options;
-      console.log(this.props.records);
-      if(this.props.records.length) {
-        options = this.props.records[this.props.d3Index].map((element) =>
-          <option key={element} value={element}>{element.toUpperCase}</option>
+      if(this.props.records) {
+        options = this.props.records.map((element) =>
+          <option key={element}>{element.toUpperCase()}</option>
         );
       }
-
-    console.log(options);
       return (
         <div>
           <select onChange={this.props.handleSetChange}>{options}</select>
--- src/components/report-module/Pie.js
+++ src/components/report-module/Pie.js
@@ -30,123 +30,73 @@ export default class Pie extends React.Component {
         records: [],
         size: 0,
         d2Index: 0,
-        d3Index: 0,
         chartCount: 0
       },
-      this.data = [];
-      this.records = [];
-      this.stats = [];
-      this.dupStats = [];
+      this.d3Index = -1;
+      this.d2Index = -1;
+      this.xxx = [];
     }
 
     componentDidMount() {
-      this.getStats();
-      //this.initPie();  // Initializes plotly data array with the records and sets title in layout
-      this.addChart(3);
+      this.setDS();
     }
 
-    initPie = () => {
-      this.setState({
-        data: [
-          {
-            values: this.stats[this.state.d2Index].values.slice(0, this.state.viewNum),
-            labels: this.stats[this.state.d2Index].labels.slice(0, this.state.viewNum),
-            type: 'pie'
-          }
-        ],
-        size: this.stats[this.state.d2Index].count
-      });
-    }
-
-    /*  return the indices where "element" exists */
-    getDupIndices = (element, d2Index) => {
-      let indices = [];
-      let index = this.dupStats[d2Index].labels.indexOf(element, 0);  // Get first index
-
-      // Continue adding the indices where "element" exists until -1 is returned -> "element" DNE in sub-array
-      while (index !== -1) {
-        indices.push(index);  // add index to indices array
-        index = this.dupStats[d2Index].labels.indexOf(element, index + 1);
-      }
-      return indices;
-    }
-
-    getD2Length = () => {
-      return this.stats[this.state.d2Index].labels.length;
-    }
-
-    getD3Length = (i) => {
-      return this.stats[i].labels.length;
-    }
-
-    getChartTitle = () => {
-      return (Object.keys(this.props.records)[this.state.d2Index]).toUpperCase();
-    }
-
-    // Create new pie chart and set location
-    createChart = (indices, i, row, column, d3Index) => {
-      let newChart = {
-        hole: 0.6,
-        textposition: 'inside',
-        values: [],
-        labels: [],
-        type: 'pie',
-        domain: {}
-      };
-
-      // Add the values and labels of the items in the same indices as the previously mentiond elemnt to a new chart which will be stored in data
-      for (let index of indices) {
-        newChart.values.push(this.stats[d3Index].values[index]);
-        newChart.labels.push(this.stats[d3Index].labels[index]);
-      }
-
-      // Set the rows and columns
-      newChart.domain.row = row;
-      newChart.domain.column = column;
-      newChart.name = this.stats[this.state.d2Index].labels[i];
-
-      return newChart;
-    }
-
-    /*  Create new annotation, set the text, its position, and style  */
-    createAnnotation = (i) => {
-      let annotation = {
-        text: this.stats[this.state.d2Index].labels[i],
-        xref: 'paper',
-        x: 1,
-        y: 0.5,
-        font: { size: 20 },
-        showarrow: false
-      };
-
-      if (i % 2 === 0)
-        annotation.x = 0;
-
-      return annotation;
-    }
-
-    /*  Add new pie chart to group  */
-    addChart = (e) => {
-      let row = 0;
-      let column = 0;
-      let rowCount = 0;
-
-      let d3Index = e;//= e.target.selectedIndex;
-
+    /*  Returns an object with the total number of records, unique labels, and each label's frequency   */
+    setDS = () => {
+      let labels = Object.keys(this.props.records); // store each of the record keys  EX: titles, id_numbers
       let i = 0;
-      let data = [];
-      let newAnnotations = [];
+      /* Iterate object of data */
+      for (let key in this.props.records) {
+        /*  Iterate data array values */
+        let dataObj = {title: key, items: []};  // Create new object which stores the set title and the labels included
+        this.xxx.push(dataObj);
+        for (let value of this.props.records[key]) {
+          let item = {label: value, count: 0, dupIndices: [], viewed: false};
+          this.xxx[i].items.push(item);
+          this.setDupIndices(item, this.xxx[i].items);
+        }
+        i += 1;
+      }
+    }
 
-      // Iterate through item in the array at d2Index
-      while ( (i < this.getD2Length()) && (i < this.getD3Length(d3Index)) ) {
-        let indices = this.getDupIndices(this.stats[this.state.d2Index].labels[i], this.state.d2Index); // Get the indices where this element exists
+    /*  Add the index of each item with same value as currItem to currItem's dupIndices property   */
+    setDupIndices = (currItem, dataArr) => {
+      for (let i = 0; i < dataArr.length; i++)
+        if ((dataArr[i].label === currItem.label)) {
+          currItem.dupIndices.push(i);
+        }
+    }
 
-        let newChart = this.createChart(indices, i, row, column, d3Index);
+    getD2Name = () => {
+      return this.xxx[this.d2Index].title;
+    }
+
+    /*  Create 3 Dimensional Pie Chart = MIND not BLOWN!!  */
+    set2DChart = (d2Index) => {
+      let data = [{labels: [], type: 'pie', name: this.getD2Name(), hoverinfo: 'label+percent+name', grid: {row: 0, column: 0}, sort: true}];
+
+      for (let item of this.xxx[d2Index].items) {
+        data[0].labels.push(item.label);
+      }
+
+      console.log(data);
+    /*  update the d3Index, row, data object and layout object  */
+    this.updateState(update(this.state, {
+      data: {$set: data},
+      layout: {title: {$set: this.getChartTitle(d2Index)}, grid: {rows: {$set: 1}, columns: {$set: 1}}, annotations: {$set: []}},
+    }));
+  }
+
+    /*  Create 3 Dimensional Pie Chart === MIND BLOWN!!  */
+    setD3Pie = (d3Index) => {
+      let row = 0, column = 0, rowCount = 0, i = 0, d2Index = this.d2Index;
+      let newAnnotations = [], data = [];
+      // Iterate through each item in the array at d2Index
+      for (let item of this.getD2Set(d2Index)) {
+        let newChart = this.newChart(item, row, column, this.xxx[d3Index].items);
         column += 1;
-
-        let annotation = this.createAnnotation(i);
-
-        // If the last item in a row was just added, go to a lower row and start at the first column
+        let annotation = this.newAnnotation(i, d2Index, d3Index);
+          // If the last item in a row was just added, go to a lower row and start at the first column
         if (column === 3) {
           column = 0;
           row += 1;
@@ -154,132 +104,119 @@ export default class Pie extends React.Component {
           annotation.x += 1;
           annotation.y += 1;
         }
-
         data.push(newChart);
         newAnnotations.push(annotation);
         i += 1;
       }
+      console.log(rowCount);
 
-      /*  update the d3Index, row, data object and layout object  */
-      this.updateState(update(this.state, {
-        d3Index: {$set: d3Index},
-        data: {$push: data},
-        layout: {title: {$set: this.getChartTitle()}, grid: {rows: {$set: rowCount}}, annotations: {$set: newAnnotations}},
-      }));
+    /*  update the d3Index, row, data object and layout object  */
+    this.updateState(update(this.state, {
+      data: {$set: data},
+      layout: {title: {$set: this.getChartTitle(d2Index)}, grid: {rows: {$set: rowCount + 1}, columns: {$set: 3}}, annotations: {$set: newAnnotations}},
+    }));
   }
 
-    // Change the number of slices in PIE chart or the number of charts appear (if 3D Pie is selected)
-    updateViewNum = (e) => {
-      this.setState({
-        viewNum: e.target.value
-      }, () => {
-        let data = [...this.state.data];
-        data.forEach((set) => {
-          set.values = this.stats[this.state.d3Index].values.slice(0, this.state.viewNum);
-          set.labels = this.stats[this.state.d3Index].labels.slice(0, this.state.viewNum);
-        });
-        this.updateState(data);
-      });
+  // Create new pie chart and set location
+  newChart = (item, row, column, d3Arr) => {
+    console.log(item);
+    let newChart = {
+      name: this.getD2Name(),
+      hoverinfo: 'label+percent+name',
+      hole: 0.6,
+      textposition: 'inside',
+      labels: [],
+      type: 'pie',
+      domain: {},
+      sort: true
+    };
+
+    /*  if this is a 3d chart, make the labels the values from d3Index at each d2Index  */
+    for (let i of item.dupIndices)
+      newChart.labels.push(d3Arr[i].label);
+    // Set the rows and columns
+    newChart.domain.row = row;
+    newChart.domain.column = column;
+    newChart.name = item.label;
+
+    return newChart;
+  }
+
+  /*  Create new annotation, set the text, its position, and style  */
+  newAnnotation = (i, d2Index, d3Index) => {
+    let annotation = {
+      text: this.xxx[d2Index].items[i].label,
+      xref: 'paper',
+      x: 1,
+      y: 0.5,
+      font: { size: 20 },
+      showarrow: false
+    };
+    if (d3Index !== -1) {
+      if (i % 2 === 0)
+        annotation.x = 0;
     }
 
-    updateState = (newState) => {
-      this.setState(newState);
+    return annotation;
+  }
+
+updateState = (newState) => {
+  this.setState(newState);
+}
+
+    getLength = (i) => {
+      return this.xxx[i].items.length;
     }
 
-    //  Returns an object with the total number of records, unique labels, and each label's frequency
-    getStats = () => {
-      for (let key in this.props.records)
-        this.records.push(this.abbr(this.props.records[key]));
-
-        for (let i in this.records) {
-          let records = this.records[i].reduce(this.countDuplicates, {});  // Stores the unique records
-
-          let dupStats = {
-            labels: [],
-            values: []
-          };
-          let j = 0;
-          for (let key of this.records[i]) {
-            dupStats.labels.push(key);
-            dupStats.values.push(records[key]);
-            j += 1;
-          }
-
-          let uniqueStats = this.statHelp(records);
-
-          this.dupStats.push(dupStats);
-          this.stats.push(uniqueStats);
-          this.records.push(records);
-        }
+    getChartTitle = (d2Index) => {
+      return Object.keys(this.props.records)[d2Index];
     }
 
-    statHelp = (records) => {
-      let count = [];
-      let sum = 0;
-      let freq = [];
-
-      for (let key in records)
-        count.push(records[key]);
-
-        sum = count.reduce((a, b) => a + b, 0);
-
-        count.forEach((value) => {
-          freq.push(value / sum);
-        });
-
-      count.reduce((a, b) => a + b, 0);
-
-      count.forEach((value) => {
-        freq.push(value / sum);
-      });
-
-      let stats = {
-        'count': Object.keys(records).length,
-        'labels': Object.keys(records),
-        'values': count,
-        'frequency': freq
-      };
-      return stats;
-    }
-
-    // Abbreviate records. 99.999 % sure this will not be needed in the future. It's just here to prevent text from blocking pie chart
-    abbr = (records) => {
-      let data = records;
-      for (let i = 0; i < data.length; i++) {
-        if (data[i].length > 15) {
-          if (data[i].indexOf(' ', 15) !== -1)
-            data[i] = data[i].slice(0, data[i].indexOf(' ', 15));
-          data[i] = data[i].concat('...');
+    /*  Returns a set with unique items  */
+    getD2Set = (d2Index) => {
+      let d2Set = [];
+      let items = this.xxx[d2Index].items;
+      // Iterate through each item
+      for (let i = 0; i < items.length; i++) {
+        let currItem = items[i];
+        let indexOfLargest = i;
+        // Check to see if there is an identical item with more duplicate indices
+        for (let j = i; j < items.length; j++)
+          if (items[j].label === currItem.label)
+            if (items[j].dupIndices.length > currItem.dupIndices.length) {
+              indexOfLargest = j;
+            }
+        if (items[indexOfLargest].viewed === false) {
+          d2Set.push(items[indexOfLargest]);
+          items[indexOfLargest].viewed = true;
         }
       }
-      return data;
-    }
-
-    countDuplicates = (obj, key) => {
-      obj[key] = (++obj[key] || 1);
-      return obj;
-    }
-
-    handleSelect = (e) => {
-      if (this.state.chartCount) {
-        this.addChart(e);
-        return;
-      }
-      this.initPie();
+      for (let item of items)
+        item.viewed = false;
+      return d2Set;
     }
 
     handleSetChange = (e) => {
-      index = e.target.selectedIndex;
-      addChart(index);
+      let index = e.target.selectedIndex;
+      // Creating 3D Chart
+      if (this.d2Index === -1) {
+        this.d2Index = index;
+        this.set2DChart(index);
+      }
+      else {
+        this.d3Index = index;
+        this.setD3Pie(index);
+      }
     }
 
     render() {
-      console.log(this.records);
+      console.log(this.state);
+      console.log(this.xxx);
       return (
         <div>
           <ChooseSet
             handleSetChange={this.handleSetChange}
-            records={this.records}
+            records={Object.keys(this.props.records)}
             index={this.state.d3Index}
           />
           <PieSlider
@@ -294,7 +231,6 @@ export default class Pie extends React.Component {
             style={this.state.style}
             />
         </div>
-
       )
     }
   }
