8ea9b7dc566ba6817a6b115fb704879e16c789cd
mdrobinson4 <mdrobinson4@crimson.ua.edu>
2018-11-05 13:45:48 -0600

    changing size via slider still breaks graph

--- src/components/report-module/Slider.js
+++ src/components/report-module/Slider.js
@@ -8,31 +8,30 @@ export default class Slider extends React.Component {
         this.state = {
           value: 0
         }
-        this.windowChange = true;
+        this.currWidth = window.innerWidth;
+        this.currHeight = window.innerHeight;
     }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-      if (this.windowChange === true) {
-        if (prevProps.width !== this.props.width) {
-          if (!(isNaN(this.props.width)) === true) {
-            this.setState(update(this.state, {value: {$set: this.props.width}})); // Update the slider with the value from the window changing
-          }
+    // Size changed by window resize 
+    componentDidUpdate(prevProps, prevState) {
+      console.log(`Width: ${this.currWidth}, Height: ${this.currHeight}`);
+      if (this.currWidth !== window.innerWidth || this.currHeight !== window.innerHeight) {
+        if (!(isNaN(this.props.width)) === true) {
+          this.setState(update(this.state, {value: {$set: Number(this.props.width)}})); // Update the slider with the value from the window changing
         }
+        this.currWidth = window.innerWidth;
+        this.currHeight = window.innerHeight;
       }
     }
 
     handleChange = e => {
       console.log(`Updating Slider Value To: ${e.target.value}`);
       this.setState(update(this.state, {value: {$set: e.target.value}}));
-      this.windowChange = false;
-      console.log(this.windowChange);
       this.props.updateSize(e);
-      console.log(this.windowChange);
-      this.windowChange = true;
     }
 
     render() {
-      console.log(this.state.value);
+      console.log(Number(this.state.value));
       return (
         <div className={styles.range_wrapper}>
           <label className={styles.label}>{this.props.label}</label>
