91a97b1154056e966ad6806af3b7889257f2cd33
mdrobinson4 <mdrobinson4@crimson.ua.edu>
2018-12-05 17:03:42 -0600

    switched to react-table and wrote function to format columns. Still having issue loading react-table.css.

--- src/components/report-module/Grid.js
+++ src/components/report-module/Grid.js
@@ -1,85 +1,60 @@
 import React from 'react'
 import Plot from 'react-plotly.js'
 import update from 'immutability-helper'
-import MultiColumnList from '@folio/stripes-components/lib/MultiColumnList';
-import { Pane, Paneset } from '@folio/stripes-components';
+import MultiColumnList from '@folio/stripes-components/lib/MultiColumnList'
+import { Pane, Paneset } from '@folio/stripes-components'
+
+// Import React Table
+import ReactTable from "react-table"
+import "./reactTable.css"
 
 export default class Grid extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: {key: 'value'}
+      data: undefined,
+      columns: []
+    };
+    // Includes the columns that we want to display
+    this.visibleColumns = {
+      Inventory: ['title', 'createdByUserId', 'createdDate', 'id', 'source'],
+      Users: ['active', 'city', 'countryId', 'region', 'postalCode', 'primaryAddress', 'preferredContactTypeId']
     };
   }
 
-  getColumns = () => {
+  // Set the columns for table
+  updateColumns = () => {
     let columns = [];
-    let table = [];
-    for (let property in this.props.longData.Inventory) {
-      columns.push({Header: property.toUpperCase(), accessor: property});
+    for (let key in this.props.data[0]) {
+      if (this.visibleColumns[this.props.title].includes(key))
+        columns.push({Header: key.toUpperCase(), accessor: key});
     }
-    table.push({Header: this.props.title, columns: columns});
-    return table;
+    this.setState({columns: columns});
   }
-/*
+
+  // Simply updates the data
+  updateData = () => {
+    this.setState({data: this.props.data});
+  }
+
   componentDidUpdate(prevProps, prevState, snapshot) {
-    if (this.props.data != undefined && this.props.data != this.state.data)
-      this.setState({data: this.props.data});
-    else if (this.props.data == undefined)
-      this.setState({data: this.props.data});
-  }
-*/
-  render() {
-
-    let catalogResults = [
-        { title: 'Biology Today',
-          id: '199930490002',
-          author: {
-            firstName: 'James',
-            lastName: 'Whitcomb',
-          },
-        },
-        { title: 'Financial Matters',
-          id: '199930490034',
-          author: {
-            firstName: 'Philip',
-            lastName: 'Marston',
-          },
-        },
-        { title: 'Modern Microbiotics',
-          id: '199930490064',
-          author: {
-            firstName: 'Eric',
-            lastName: 'Martin',
-          },
-        },
-      ];
-
-      //catalogResults = undefined;
-
-
-    console.log(this.state.data);
-    console.log(this.state.title);
-    let visibleColumns = [];
-
-    switch (this.props.title) {
-      case 'Users':
-        visibleColumns = ['active', 'city', 'countryId', 'region', 'postalCode', 'primaryAddress', 'preferredContactTypeId'];
-        break;
-      case 'Inventory':
-        visibleColumns = ['createdByUserId', 'createdDate', 'id', 'source', 'title', 'updatedDate', 'value'];
-        break;
-      default:
-        visibleColumns = null;
+    if ((this.props.data !== undefined && this.props.data != prevProps.data)) {
+      this.updateData();
+      this.updateColumns();
     }
-    console.log(visibleColumns);
+    else if (this.props.data != prevProps.data)  {
+      this.setState({data: this.props.data, columns: []});
+    }
+  }
+
+  render() {
     return (
-      <div>
-        <MultiColumnList
-            contentData={catalogResults}
-            visibleColumns={['title', 'id']}
-        />
-			</div>
+          <ReactTable
+            data={this.state.data}
+            columns={this.state.columns}
+            defaultPageSize={10}
+            className="-striped -highlight"
+          />
     )
   }
 }
